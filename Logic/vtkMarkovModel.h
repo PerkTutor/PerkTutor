bbb1b7cb564fe5e6dad635176375690dba396f2d
mholden8 <mholden8@35aa5c59-fbbe-4c73-a865-bce86c728669>
2012-11-09 17:27:14 +0000

    Added Markov Model algorithms for workflow segmentation algorithm step 5.
    git-svn-id: https://subversion.assembla.com/svn/slicerigt/trunk/Slicer4/WorkflowSegmentation@181 35aa5c59-fbbe-4c73-a865-bce86c728669

---
+++ Logic/vtkMarkovModel.h
@@ -0,0 +1,68 @@
+
+#ifndef MARKOVMODEL_H
+#define MARKOVMODEL_H
+
+#include "RecordType.h"
+#include "vtkObject.h"
+#include "vtkSlicerWorkflowSegmentationModuleLogicExport.h"
+
+#include <vector>
+#include <iostream>
+
+// Class representing a procedure comprised of tracking records
+class VTK_SLICER_WORKFLOWSEGMENTATION_MODULE_LOGIC_EXPORT
+  vtkMarkovModel : public vtkObject
+{
+public:
+
+  static vtkMarkovModel *New();
+
+  vtkMarkovModel* DeepCopy();
+
+  void SetSize( int numNewStates, int numNewSymbols );
+  int GetNumStates();
+  int GetNumSymbols();
+
+  std::vector<LabelRecord> GetA();
+  std::vector<LabelRecord> GetZeroA();
+  std::vector<LabelRecord> GetLogA();
+
+  std::vector<LabelRecord> GetB();
+  std::vector<LabelRecord> GetZeroB();
+  std::vector<LabelRecord> GetLogB();
+
+  LabelRecord GetPi();
+  LabelRecord GetZeroPi();
+  LabelRecord GetLogPi();
+
+
+  void InitializeEstimation( int numEstStates, int numEstSymbols );
+  void AddEstimationData( std::vector<MarkovRecord> sequence );
+  void AddPseudoData( LabelRecord pseudoPi, std::vector<LabelRecord> pseudoA, std::vector<LabelRecord> pseudoB );
+  void EstimateParameters();
+  std::vector<MarkovRecord> CalculateStates( std::vector<MarkovRecord> sequence );
+  
+
+private:
+
+	void ZeroParameters();
+	void NormalizeParameters();
+
+public:
+
+  vtkMarkovModel();
+  vtkMarkovModel( int numInitStates, int numInitSymbols );
+  ~vtkMarkovModel();
+
+private:
+
+	std::vector<LabelRecord> A; // State transition matrix
+	std::vector<LabelRecord> B; // Observation matrix
+	LabelRecord pi;	// Initial state vector
+
+	int numStates;
+	int numSymbols;
+
+};
+
+#endif
